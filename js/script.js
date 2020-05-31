dee86f365afdf606df00ba301c2e2097f9c40d85
MilosBogicevic <milbog.development@gmail.com>
2020-05-31 19:22:45 +0200

    JS done.

--- js/script.js
+++ js/script.js
@@ -7,9 +7,26 @@ button.addEventListener("click", newColor);
 
 // Functions
 function newColor() {
-  let randomColor = "#" + Math.floor(Math.random() * 10000000).toString(16);
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   //   Apply background color
   document.body.style.backgroundColor = randomColor;
   //   Replace h2 text with new color
   color.innerText = randomColor;
+}
+
+function copyColor() {
+  let text = color.innerText;
+  //   Create input element
+  let input = document.createElement("input");
+  input.setAttribute("value", text);
+  //   Append input element
+  document.body.appendChild(input);
+  //   Select all text in an input field
+  input.select();
+  //   Replace clipboard's current content
+  document.execCommand("copy");
+  //   Remove input element
+  document.body.removeChild(input);
+
+  alert("Copied: " + text);
 }
